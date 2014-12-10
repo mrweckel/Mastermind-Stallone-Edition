4687b25e177c31b12c66bec22f6446b8a480bb13
mrweckel <mrweckel@gmail.com>
2014-12-10 12:09:36 -0500

    add answer file and functions

---
+++ javascripts/mastermind-answer.js
@@ -0,0 +1,9 @@
+Mastermind.Answer = function(){
+  COLORS = ["aqua.png","blue.png","green.png","orange.png","purple.png","yellow.png"]
+}
+
+Mastermind.Answer.prototype = {
+  setAnswer: function(){
+    //set answer based on random selection of COLORS
+  }
+}
