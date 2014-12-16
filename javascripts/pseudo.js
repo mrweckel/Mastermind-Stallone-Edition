2b25e0658d08aef78efdffed2b27d05a90d68fbd
mrweckel <mrweckel@gmail.com>
2014-12-16 16:00:58 +0700

    update pseudo

--- javascripts/pseudo.js
+++ javascripts/pseudo.js
@@ -19,7 +19,7 @@
 //Have choices of color placed on the side of the page and user can drag and drop over to board. for now-- once they complete a row the response will automatically be given.
   //once row is completed, array will be created.
   //that array will be checked against the main array
-      //1st for correct color and location. Then those colors will be pushed from array, and thn
+      //1st for correct color and location. Then those colors will be pushed from array, and then
       //2nd for correct color only checked against remaining pegs in the array
 
 // Create  a run page that will call all of the methods being created in my OOJS world.
