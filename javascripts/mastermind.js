17412bef90eb7ba9d9831b94c9e5304939c4e852
mrweckel <mrweckel@gmail.com>
2014-12-21 17:22:35 +0700

    add mastermind file to control actions after DOM is loaded

---
+++ javascripts/mastermind.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function(){
+  window.colorSet = new Mastermind.PegColors();
+  document.getElementById("new-game").onclick = function(){
+    window.answer = new Mastermind.Answer(window.colorSet.colors);
+  }
+
+});
