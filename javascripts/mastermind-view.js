dbc2e784e6a4c9b8e9decd51a7152cd9f8c80c38
mrweckel <mrweckel@gmail.com>
2014-12-22 13:08:43 +0700

    create showanswer for view

--- javascripts/mastermind-view.js
+++ javascripts/mastermind-view.js
@@ -3,5 +3,14 @@ Mastermind.View = function(){
 }
 
 Mastermind.View.prototype = {
+  showAnswer: function(answer){
+    for(var i=0; i<answer.length; i++){
+      this.node = document.createElement("td");
+      this.img  = document.createElement("img");
 
+      this.img.src = "imgs/"+ answer[i];
+      this.node.appendChild(this.img);
+      document.getElementById ("answer-tr").appendChild(this.node);
+    }
+  }
 }
