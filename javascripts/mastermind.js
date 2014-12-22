8e3feddcdf471abea0f8fe84e9cb88e0ea56d671
mrweckel <mrweckel@gmail.com>
2014-12-22 13:09:02 +0700

    add controller and view to main file

--- javascripts/mastermind.js
+++ javascripts/mastermind.js
@@ -1,8 +1,12 @@
 document.addEventListener('DOMContentLoaded', function(){
+  window.controller = new Mastermind.Controller();
+  window.view = (new Mastermind.View());
   window.colorSet = new Mastermind.PegColors();
   document.getElementById("new-game").onclick = function(){
     window.answer = new Mastermind.Answer(window.colorSet.colors);
     window.answer.setAnswer();
+
+    window.view.showAnswer(window.answer.current_ans);
   }
 
 });
