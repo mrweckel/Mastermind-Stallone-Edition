ba69867803217783c09f2256bfd6fed9ad307686
mrweckel <mrweckel@gmail.com>
2014-12-22 11:25:05 +0700

    setAnswer now working

--- javascripts/mastermind-answer.js
+++ javascripts/mastermind-answer.js
@@ -6,13 +6,14 @@ Mastermind.Answer.prototype = {
   current_ans: [],
 
   addPeg: function(){
-    var peg = arr[Math.floor(Math.random() * arr.length)];
-    return current_ans.push(peg);
+    var peg = this.arr[Math.floor(Math.random() * this.arr.length)];
+    return this.current_ans.push(peg);
   },
 
   setAnswer: function(){
     for(var i=0;i<4;i++){
-      this.addPeg();
+      this.addPeg(this.arr);
     }
+    return this.current_ans
   }
 }
--- javascripts/mastermind.js
+++ javascripts/mastermind.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function(){
   window.colorSet = new Mastermind.PegColors();
   document.getElementById("new-game").onclick = function(){
     window.answer = new Mastermind.Answer(window.colorSet.colors);
+    window.answer.setAnswer();
   }
 
 });
