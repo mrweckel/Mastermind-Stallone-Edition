3c4a58c5ecafe7f1b74b881722cff80392acf66c
mrweckel <mrweckel@gmail.com>
2015-01-22 12:20:31 +0800

    add showObjects

--- javascripts/mastermind-view.js
+++ javascripts/mastermind-view.js
@@ -22,5 +22,16 @@ Mastermind.View.prototype = {
       node.removeChild(child);
       child = node.firstChild;
     }
+  },
+
+  showObjects: function(options){
+    for(var i=0;i<options.length;i++){
+      this.node = document.createElement("td");
+      this.img = document.createElement("img")
+
+      this.img.src = options[i].img;
+      this.node.appendChild(this.img);
+      document.getElementById("options-tr").appendChild(this.node);
+    };
   }
 }
--- javascripts/mastermind.js
+++ javascripts/mastermind.js
@@ -6,15 +6,20 @@ document.addEventListener('DOMContentLoaded', function(){
   window.colorSet = new Mastermind.PegColors();
   window.colorSetObjects = window.colorSet.createObjects();
 
+  window.options = window.view.showObjects(window.colorSet.colorObjects);
+
 
   document.getElementById("new-game").onclick = function(){
       window.view.clearAnswer("answer-tr");
+
 
       window.answer = new Mastermind.Answer(window.colorSet.colorObjects);
 
       window.answer.setAnswer();
 
       window.view.showAnswer(window.answer.current_ans);
+
+      window.guess = new Mastermind.Guess;
   }
 
 });
