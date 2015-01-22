87741f0a0473eb048c5d6349fcb7403db2b90d2d
mrweckel <mrweckel@gmail.com>
2015-01-22 20:00:19 +0800

    WIP--adding node to peg

--- javascripts/mastermind-peg.js
+++ javascripts/mastermind-peg.js
@@ -1,16 +1,11 @@
 Mastermind.Peg = function(color){
   this.color = color;
   this.img   = "imgs/" + color + ".png";
+  // adding an element to be able to set onclick.needed?
+  this.node = document.createElement("span");
 }
 
 Mastermind.Peg.prototype = {
-  createNode: function(){
-    this.node = document.createElement("span")
-  },
-
-  pegClick: function(){
-
-  }
 
 }
 
