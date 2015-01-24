e690453b83507e60c18734adb5f7ce9361aae4da
mrweckel <mrweckel@gmail.com>
2015-01-24 21:23:34 +0800

    checkforwin -- WIP

--- javascripts/mastermind.js
+++ javascripts/mastermind.js
@@ -25,18 +25,23 @@ document.addEventListener('DOMContentLoaded', function(){
       var board_body = document.getElementById("board-body");
 
       //this needs to change when I get internet
-      var curr_row = board_body.lastChild.id;
-      var last_char = curr_row.length -1
-      var curr_row_num = parseInt(curr_row[last_char]);
+      var curr_row = parseInt(board_body.lastChild.id);
+
+      //check for end of game
+      window.game.endOfGame(curr_row, 12)
 
       if (window.guess.guess.length < 4) {
         window.guess.guess.push(new_peg);
-        window.view.appendPegToGuess(new_peg, curr_row_num);
+        window.view.appendPegToGuess(new_peg, curr_row);
+
+        //not working!!!!!!!!!!!!
+        window.game.checkForWin(window.guess.guess, window.answer.current_ans, "color", window.game.won)
       } else {
+
         window.guess.clearGuess(window.guess.guess);
         window.guess.guess.push(new_peg);
 
-        var new_row_num = curr_row_num + 1
+        var new_row_num = curr_row + 1
         window.view.createRow("tr", new_row_num);
         window.view.appendPegToGuess(new_peg, new_row_num);
       };
