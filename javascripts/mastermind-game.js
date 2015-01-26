7143f7b37957b531d0276a8ab3f43ca487466bf7
mrweckel <mrweckel@gmail.com>
2015-01-26 12:15:26 +0800

    add checkForCorrectness-- combines Postion and ColorMatch check

--- javascripts/mastermind-game.js
+++ javascripts/mastermind-game.js
@@ -17,9 +17,41 @@ Mastermind.Game.prototype = {
       }
     }
     if(winner === true && guess_arr.length === 4){
-      console.log("You have won");
+      //temporary
+      alert("You have won");
     }
   },
+
+  checkForCorrectness: function(guess_arr, ans_arr, prop){
+    var destructive_arr = guess_arr;
+
+    this.checkForPositionMatch(destructive_arr, ans_arr, prop);
+    this.checkForColorMatch(destructive_arr, ans_arr, prop);
+  },
+
+  checkForPositionMatch: function(guess_arr, ans_arr, prop){
+    for(var i=0; i<guess_arr.length; i++){
+      if(guess_arr[i][prop] === ans_arr[i][prop]){
+        console.log("Position" + i + ": Correct" );
+        guess_arr.splice(i, 1, "")
+        break;
+      }
+    }
+    return guess_arr;
+  },
+
+  checkForColorMatch: function(guess_arr, ans_arr, prop){
+    var match = true;
+    for(var i=0; i<guess_arr.length; i++){
+      for(var j=0; j<ans_arr.length; j++){
+        if(guess_arr[i][prop] === ans_arr[j][prop] && i != j){
+          console.log((i + 1) + "Color is Correct" );
+          break
+        }
+      }
+    }
+  },
+
 
   won: function(){
     console.log("You have won")
