6f61804c25dfc24cb0178540276dc7d10fd9beec
mrweckel <mrweckel@gmail.com>
2015-01-26 12:40:40 +0800

    add correctness_arr as argument

--- javascripts/mastermind-game.js
+++ javascripts/mastermind-game.js
@@ -22,30 +22,31 @@ Mastermind.Game.prototype = {
     }
   },
 
-  checkForCorrectness: function(guess_arr, ans_arr, prop){
+  checkForCorrectness: function(guess_arr, ans_arr, prop, correctness_arr){
     var destructive_arr = guess_arr;
 
-    this.checkForPositionMatch(destructive_arr, ans_arr, prop);
-    this.checkForColorMatch(destructive_arr, ans_arr, prop);
+    this.checkForPositionMatch(destructive_arr, ans_arr, prop, correctness_arr);
+    this.checkForColorMatch(destructive_arr, ans_arr, prop, correctness_arr);
   },
 
-  checkForPositionMatch: function(guess_arr, ans_arr, prop){
+  checkForPositionMatch: function(guess_arr, ans_arr, prop, correctness_arr){
     for(var i=0; i<guess_arr.length; i++){
       if(guess_arr[i][prop] === ans_arr[i][prop]){
         console.log("Position" + i + ": Correct" );
-        guess_arr.splice(i, 1, "")
-        break;
+        guess_arr.splice(i, 1, "place_holder");
+        this.addIndicator(correctness_arr, new Mastermind.Peg("red"));
       }
     }
     return guess_arr;
   },
 
-  checkForColorMatch: function(guess_arr, ans_arr, prop){
+  checkForColorMatch: function(guess_arr, ans_arr, prop, correctness_arr){
     var match = true;
     for(var i=0; i<guess_arr.length; i++){
       for(var j=0; j<ans_arr.length; j++){
         if(guess_arr[i][prop] === ans_arr[j][prop] && i != j){
           console.log((i + 1) + "Color is Correct" );
+          this.addIndicator(correctness_arr, new Mastermind.Peg("white"));
           break
         }
       }
