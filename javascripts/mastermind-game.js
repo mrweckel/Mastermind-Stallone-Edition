0c8e0cc81384cd3ddf5dd5e554dc9d5f8476c40f
mrweckel <mrweckel@gmail.com>
2015-02-03 21:10:54 +0800

    change destructiveness to act on ans_arr---not working

--- javascripts/mastermind-game.js
+++ javascripts/mastermind-game.js
@@ -23,36 +23,39 @@ Mastermind.Game.prototype = {
   },
 
   checkGuess: function(guess_arr, ans_arr, prop, correctness_arr){
-    var destructive_arr = guess_arr;
+
 
     this.checkForWin(guess_arr, ans_arr, prop, this.won);
-    this.checkForPositionMatch(destructive_arr, ans_arr, prop, correctness_arr);
-    this.checkForColorMatch(destructive_arr, ans_arr, prop, correctness_arr);
+    this.checkForPositionMatch(guess_arr, ans_arr, prop, correctness_arr);
+    this.checkForColorMatch(guess_arr, ans_arr, prop, correctness_arr);
   },
 
-  checkForPositionMatch: function(guess_arr, ans_arr, prop, correctness_arr){
+  checkForPositionMatch: function(guess_arr, ans_arr_holder, prop, correctness_arr){
+    var destructive_arr = ans_arr_holder;
     for(var i=0; i<guess_arr.length; i++){
-      if(guess_arr[i][prop] === ans_arr[i][prop]){
+      if(guess_arr[i][prop] === destructive_arr[i][prop]){
         console.log("Position" + i + ": Correct" );
-        guess_arr.splice(i, 1, "place_holder");
+        debugger;
+        //splice is not working
+        destructive_arr.splice(i, 1, "place_holder");
         this.addIndicator(correctness_arr, new Mastermind.Peg("red"));
       }
     }
-    return guess_arr;
+    return destructive_arr;
   },
 
-  checkForColorMatch: function(guess_arr, ans_arr, prop, correctness_arr){
+  checkForColorMatch: function(guess_arr, ans_arr_holder, prop, correctness_arr){
     var match = true;
     for(var i=0; i<guess_arr.length; i++){
-      for(var j=0; j<ans_arr.length; j++){
-        if(guess_arr[i][prop] === ans_arr[j][prop] && i != j){
+      for(var j=0; j<ans_arr_holder.length; j++){
+        if(guess_arr[i][prop] === ans_arr_holder[j][prop] && i != j){
           console.log((i + 1) + "Color is Correct" );
           this.addIndicator(correctness_arr, new Mastermind.Peg("white"));
           break
         }
       }
     }
-    return guess_arr;
+    // return ans_arr_holder;
   },
 
   addIndicator: function(arr, peg){
