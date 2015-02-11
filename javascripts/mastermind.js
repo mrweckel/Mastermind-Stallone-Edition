af3f82bce99b7ac8f4e45f502e671442986c359c
mrweckel <mrweckel@gmail.com>
2015-02-10 19:11:52 -0500

    remove .controller from mastermind.js

--- javascripts/mastermind.js
+++ javascripts/mastermind.js
@@ -2,38 +2,30 @@ document.addEventListener('DOMContentLoaded', function(){
 
   window.controller = new Mastermind.Controller();
 
-  window.controller.defineView(new Mastermind.View());
+  window.view = window.controller.defineView(new Mastermind.View());
 
-  window.view = window.controller.view;
+  window.guess = window.controller.defineGuess(new Mastermind.Guess());
 
-  window.controller.defineGuess(new Mastermind.Guess());
-
-  window.guess =  window.controller.guess;
+  window.game = window.controller.defineGame(new Mastermind.Game());
 
 //Peg Option Creation and Display
   window.colorSet = new Mastermind.PegColors();
 
-  //creates the pegs from color string
+//creates the pegs from color string
   window.colorSetObjects = window.colorSet.createObjects();
 
   window.options = window.view.showObjects(window.colorSet.colorObjects, window.controller.makeGuess);
 
-
-  window.game  = new Mastermind.Game;
-
-  window.indicators = new Mastermind.Indicator;
-
-
-
+  window.indicators = window.controller.defineIndicators(new Mastermind.Indicator());
 
 //ANSWER functionality
-    window.controller.view.clearAnswer("answer-tr");
+    window.view.clearAnswer("answer-tr");
 
     window.answer = new Mastermind.Answer(window.colorSet.colorObjects);
 
     window.answer.setAnswer();
 
-    window.controller.view.showAnswer(window.answer.current_ans);
+    window.view.showAnswer(window.answer.current_ans);
 
 
 //After game is over
